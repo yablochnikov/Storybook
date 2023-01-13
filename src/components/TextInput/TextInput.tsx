4bf9bcd42413e7c908803f58292d2963259d55e0
Denis Sidletskyy <Denis.Sidletskyy@fivesysdev.com>
2023-01-13 11:33:17 +0200

    TextInput component

---
+++ src/components/TextInput/TextInput.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable no-restricted-globals */
+import { InputAdornment, SxProps, TextField as MuiTextInput } from '@mui/material';
+import { ChangeEvent, FC, ReactNode, Ref } from 'react';
+
+type TextFieldVariant = 'outlined' | 'filled' | 'standard';
+type TextInputColor = 'primary' | 'secondary' | 'info' | 'error' | 'success' | 'warning';
+type TextInputType = 'text' | 'number' | 'password' | 'search' | 'date' | 'time';
+type TextInputSize = 'medium' | 'small';
+type TextInputMargin = 'none' | 'normal' | 'dense';
+
+export interface ITextInput {
+	variant?: TextFieldVariant;
+	size?: TextInputSize;
+	color?: TextInputColor;
+	type?: TextInputType;
+	margin?: TextInputMargin;
+	label?: string;
+	placeholder?: string;
+	helperText?: string;
+	error?: boolean;
+	disabled?: boolean;
+	autoFocus?: boolean;
+	fullWidth?: boolean;
+	required?: boolean;
+	multiline?: boolean;
+	rows?: number;
+	minRows?: number;
+	maxRows?: number;
+	id?: string;
+	name?: string;
+	value?: string;
+	onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+	startAdornment?: ReactNode;
+	endAdornment?: ReactNode;
+	sx?: SxProps;
+	inputRef?: Ref<HTMLInputElement>;
+}
+
+export const TextInput: FC<ITextInput> = ({ label, type, endAdornment, startAdornment, ...props }) => {
+	return (
+		<MuiTextInput
+			{...props}
+			hiddenLabel={!label}
+			label={!['date', 'time'].includes(type!) ? label : undefined}
+			type={type}
+			InputProps={{
+				startAdornment: startAdornment && <InputAdornment position="start">{startAdornment}</InputAdornment>,
+				endAdornment: endAdornment && <InputAdornment position="end">{endAdornment}</InputAdornment>,
+			}}
+		/>
+	);
+};
