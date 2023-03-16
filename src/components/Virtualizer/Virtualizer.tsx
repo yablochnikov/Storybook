5502edf7b9ddf890bfb429d006b9d96b18599417
Nikita Yablochnikov <110604421+yablochnikov@users.noreply.github.com>
2023-03-16 13:28:31 +0200

    fixes after review

--- src/components/Virtualizer/Virtualizer.tsx
+++ src/components/Virtualizer/Virtualizer.tsx
@@ -1,5 +1,5 @@
 import uniqid from 'uniqid';
-import { Box } from '@mui/material';
+import { Box, SxProps } from '@mui/material';
 import useVirtualizedList from '../../hooks/useVirtualList';
 import { VirtualizedListProps } from '../../core/models/virtualizer';
 
@@ -15,32 +15,31 @@ const VirtualizedList = <T,>({ items, itemSize, renderItem, overScan = 10, type 
 	const isVertical = type === 'vertical';
 
 	const visibleItems = items.slice(startIndex, endIndex);
-	const containerStyle = isVertical
+	const containerStyle: SxProps = isVertical
 		? { height: '100vh', overflowY: 'scroll' }
 		: { width: '100vw', overflowX: 'scroll', whiteSpace: 'nowrap' };
 
-	const spacerStyle = isVertical
+	const spacerStyle: SxProps = isVertical
 		? { height: `${startIndex * itemSize}px` }
 		: { width: `${startIndex * itemSize}px`, display: 'inline-block' };
 
-	const itemStyle = isVertical
+	const itemStyle: SxProps = isVertical
 		? { marginBottom: '10px' }
 		: { height: '100%', display: 'inline-block', verticalAlign: 'top', marginLeft: '10px' };
 
-	const scrollStyle = isVertical
+	const scrollStyle: SxProps = isVertical
 		? { height: `${(itemCount - endIndex) * itemSize}px` }
 		: { width: `${(itemCount - endIndex) * itemSize}px`, height: `${itemSize}px` };
 
 	return (
-		// @ts-ignore
 		<Box ref={listRef} sx={containerStyle} onScroll={handleScroll}>
 			<Box sx={spacerStyle} />
 			{visibleItems.map(item => (
-				<Box key={uniqid()} style={itemStyle}>
+				<Box key={uniqid()} sx={itemStyle}>
 					{renderItem(item)}
 				</Box>
 			))}
-			<Box style={scrollStyle} />
+			<Box sx={scrollStyle} />
 		</Box>
 	);
 };
